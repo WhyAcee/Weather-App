a229f1741b306c465a0e33923b765106cf04c1a3
Marlon <marlonwason100@gmail.com>
2023-07-21 18:20:34 -0400

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        ],
-    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
