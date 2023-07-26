7fce1680796c56f5f25e4c6c86eacecb95b3ef1d
Marlon <marlonwason100@gmail.com>
2023-07-25 20:19:50 -0400

    add weather updating

--- src/index.js
+++ src/index.js
@@ -5,17 +5,28 @@ const submitBtn = document.getElementById('submit-btn');
 const key = '77c6d40a83274e6ab4b144221231907';
 let location = 'toronto';
 
+function updateWeather(weather) {
+  const conditionElement = document.querySelector('.condition');
+  const locationElement = document.querySelector('.location');
+  const tempElement = document.querySelector('.temp');
+  const feelsLikeElement = document.querySelector('.feels-like');
+  const humidityElement = document.querySelector('.humidity');
+  const visibilityElement = document.querySelector('.visibility');
+
+  console.log(weather);
+  conditionElement.textContent = weather.current.condition.text;
+  locationElement.textContent = `${weather.location.name}, ${weather.location.country}`;
+  tempElement.textContent = Math.round(weather.current.temp_c);
+  feelsLikeElement.textContent = `Feels like: ${Math.round(weather.current.feelslike_c)}`;
+  humidityElement.textContent = `Humidity: ${weather.current.humidity}%`;
+  visibilityElement.textContent = `Visibility: ${weather.current.vis_km}km`;
+}
+
 async function fetchCurrentWeather(city) {
   try {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`, { mode: 'cors' });
     const weather = await response.json();
-    console.log(weather);
-    console.log(weather.current.condition.text);
-    console.log(`${weather.location.name}, ${weather.location.country}`);
-    console.log(weather.current.temp_c);
-    console.log(`Feels like: ${weather.current.feelslike_c}`);
-    console.log(`Humidity: ${weather.current.humidity}%`);
-    console.log(`Visibility: ${weather.current.vis_km}km`);
+    updateWeather(weather);
   } catch (error) {
     console.log(error);
   }
